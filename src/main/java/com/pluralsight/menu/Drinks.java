11b75acb7a8a868ca810abc7279982be48fae137
polinabreha <polinabreha@gmail.com>
2026-05-22 12:35:04 -0400

    created the drinks class

--- src/main/java/com/pluralsight/menu/Drinks.java
+++ src/main/java/com/pluralsight/menu/Drinks.java
@@ -1,30 +1,41 @@
 package com.pluralsight.menu;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class Drinks {
-    private List<String> drinkName;
-    private double price;
-    private int size;
-    public Drinks(List<String> drinkName, double price, int size) {
-        this.drinkName = drinkName;
-        this.price = price;
+public class Drinks implements MenuItem {
+    private String name;
+    private String size;
+
+    public Drinks(String name,  String size) {
+        this.name = name;
         this.size = size;
     }
-    public List<String> getDrinkName() {
-        ArrayList<String> drinkName = new ArrayList<>();
-        drinkName.add("Water");
-        drinkName.add("Orange juice");
-        drinkName.add("Coca-Cola");
-        drinkName.add("Diet Coca-Cola");
-        drinkName.add("Root Beer");
-        drinkName.add("Cream soda");
-        drinkName.add("Poppi probiotic drinks");
-        drinkName.add("Kombucha");
-        drinkName.add("Apple juice");
-        return drinkName;
+//    public List<String> getDrinkName() {
+//        ArrayList<String> drinkName = new ArrayList<>();
+//        drinkName.add("Water");
+//        drinkName.add("Orange juice");
+//        drinkName.add("Coca-Cola");
+//        drinkName.add("Diet Coca-Cola");
+//        drinkName.add("Root Beer");
+//        drinkName.add("Cream soda");
+//        drinkName.add("Poppi probiotic drinks");
+//        drinkName.add("Kombucha");
+//        drinkName.add("Apple juice");
+//        return drinkName;
+//    }
+
+    @Override
+    public String getName() {
+        return this.name  + " " + this.size;
     }
 
-
+    @Override
+    public double getPrice() {
+        return switch (size.toLowerCase()){
+            case "small" -> 2.00;
+            case "medium" -> 2.50;
+            case "large" -> 3.00;
+            default -> throw new IllegalArgumentException ("Invalid size");
+        };
+    }
 }
