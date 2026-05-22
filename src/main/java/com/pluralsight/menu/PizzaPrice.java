20f4e45c8202b4763917a4cf9029d53c6520fce0
polinabreha <polinabreha@gmail.com>
2026-05-22 11:44:20 -0400

    improved the PizzaPrice logic

--- src/main/java/com/pluralsight/menu/PizzaPrice.java
+++ src/main/java/com/pluralsight/menu/PizzaPrice.java
@@ -3,6 +3,14 @@ package com.pluralsight.menu;
 
 public class PizzaPrice {
    public static double calculatePizzaPrice (Pizza pizza) {
+
+       int meatCount = pizza.getMeats().size();
+       int cheeseCount = pizza.getCheese().size();
+
+       int extraMeat = Math.max(0, meatCount - 1);
+       int extraCheese = Math.max(0,cheeseCount);
+
+
        double basePrice = switch(pizza.getSize()){
            case 8 -> 8.50;
            case 12 -> 12;
@@ -10,34 +18,42 @@ public class PizzaPrice {
            default -> throw new IllegalArgumentException("Invalid pizza size");
        };
 
-       double meatPrice = pizza.getMeats().size() * switch(pizza.getSize()){
-           case 8 -> 1;
-           case 12 -> 2;
-           case 16 -> 3;
-           default -> 0;
-       };
+       double meatPrice = 0;
 
-       double cheesePrice = pizza.getCheese().size() * switch (pizza.getSize()){
+       if (meatCount > 0) {
+          meatPrice += switch(pizza.getSize()) {
+              case 8 -> 1.00;
+              case 12 -> 2.00;
+              case 16 -> 3.00;
+              default -> 0;
+          };
+       }
+       double cheesePrice = 0;
+
+        if (cheeseCount > 0) {
+          cheesePrice += switch (pizza.getSize()){
            case 8 -> 0.75;
            case 12 -> 1.50;
            case 16 -> 2.25;
            default -> 0;
-       };
+          };
 
-       double extraMeatPrice = pizza.getExtraMeatCounts() * switch(pizza.getSize()){
+       }
+
+       meatPrice += extraMeat * switch(pizza.getSize()){
            case 8 -> 0.50;
            case 12 -> 1;
            case 16 -> 1.50;
            default -> 0;
        };
 
-       double extraCheesePrice = pizza.getExtraCheeseCounts() * switch (pizza.getSize()){
+       cheesePrice += extraCheese * switch (pizza.getSize()){
            case 8 -> 0.30;
            case 12 -> 0.60;
            case 16 -> 0.90;
            default -> 0;
        };
-       return basePrice + meatPrice + cheesePrice + extraMeatPrice + extraCheesePrice;
+       return basePrice + meatPrice + cheesePrice ;
    }
 
 }
