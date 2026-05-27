606369924361ff96eb1ed906346a0b77bd6a242c
polinabreha <polinabreha@gmail.com>
2026-05-27 08:50:09 -0400

    created the signature pizza class

---
+++ src/main/java/com/pluralsight/menu/HawaiianPizza.java
@@ -0,0 +1,24 @@
+package com.pluralsight.menu;
+
+public abstract class HawaiianPizza extends CustomPizza{
+    public HawaiianPizza(int size) {
+        super(size);
+
+        addMeat("ham");
+        addCheese("mozzarella");
+        addCrust("thin");
+        addToppings("pineapple");
+        addSauces("marinara");
+        addSides("parmesan");
+    }
+
+    @Override
+    public String getName() {
+        return "Hawaiian Pizza";
+    }
+
+    @Override
+    public double getPrice() {
+        return PizzaPrice.calculatePizzaPrice(this);
+    }
+}
